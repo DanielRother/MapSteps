46b99844edeecf31f41877df66f7fadb39530409
Daniel Rother <daniel_rother@gmx.de>
2023-08-10 00:15:38 +0200

    chore: beautify language selector

--- pages/components/country-selector.js
+++ pages/components/country-selector.js
@@ -10,135 +10,96 @@ import { groupBy } from "../../utils/utils.js";
 const { Option, OptGroup } = Select;
 
 export default function CountrySelector() {
-  const [selected, setSelected] = useState();
+    const [selected, setSelected] = useState();
 
-  const { t } = useTranslation();
+    const { t } = useTranslation();
+    const locale = t("locale");
 
-  const locale = t("locale");
-  i18n.locales.forEach((l) => {
-    countries.registerLocale(
-      require("i18n-iso-countries/langs/" + l + ".json"),
-    );
-  });
+    i18n.locales.forEach((l) => {
+        countries.registerLocale(require("i18n-iso-countries/langs/" + l + ".json"));
+    });
 
-  let c = countries.getNames(locale);
-  let countriesByContinents = groupBy(
-    Object.values(countries.getNames(locale)),
-    (c) => {
-      let cc = getContinentCode(countries.getAlpha2Code(c, locale));
-      return cc;
-    },
-  );
+    let c = countries.getNames(locale);
+    let countriesByContinents = groupBy(Object.values(countries.getNames(locale)), (c) => {
+        let cc = getContinentCode(countries.getAlpha2Code(c, locale));
+        return cc;
+    });
 
-  const onChange = (value) => {
-    console.log(`selected ${value}`);
-    setSelected(value);
-  };
-
-  const clearSelected = () => {
-    setSelected([]);
-  };
-
-  const flagTagRender = (props) => {
-    const { label, value, closable, onClose } = props;
-    const onPreventMouseDown = (event) => {
-      event.preventDefault();
-      event.stopPropagation();
+    const onChange = (value) => {
+        console.log(`selected ${value}`);
+        setSelected(value);
     };
-    const countryCode = countries.getAlpha2Code(label, locale);
-    var flag = <ReactCountryFlag countryCode={countryCode} />;
+
+    const clearSelected = () => {
+        setSelected([]);
+    };
+
+    const flagTagRender = (props) => {
+        const { label, value, closable, onClose } = props;
+        const onPreventMouseDown = (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+        };
+        const countryCode = countries.getAlpha2Code(label, locale);
+        var flag = <ReactCountryFlag countryCode={countryCode} />;
+        return (
+            <Tag onMouseDown={onPreventMouseDown} closable={closable} onClose={onClose} icon={flag}>
+                {label}
+            </Tag>
+        );
+    };
+
+    const addCountryOptionsForContinent = (name, countriesForContinent) => {
+        return (
+            <>
+                <OptGroup label={name} key={name}>
+                    {countriesForContinent.map((v) => {
+                        const alpha2 = countries.getAlpha2Code(v, locale).toLocaleLowerCase();
+                        const alpha3 = countries.getAlpha3Code(v, locale).toLocaleLowerCase();
+                        const flag = <ReactCountryFlag countryCode={alpha2} />;
+
+                        return (
+                            <>
+                                <Option value={alpha3} label={v} key={alpha3}>
+                                    <Space>
+                                        <span role="img" aria-label={alpha3}>
+                                            {flag}
+                                        </span>
+                                        {v}
+                                    </Space>
+                                </Option>
+                            </>
+                        );
+                    })}
+                </OptGroup>
+            </>
+        );
+    };
+
     return (
-      <Tag
-        // color={value}
-        onMouseDown={onPreventMouseDown}
-        closable={closable}
-        onClose={onClose}
-        // style={{
-        //   marginRight: 3,
-        // }}
-        icon={flag}
-      >
-        {label}
-      </Tag>
+        <div>
+            <Select
+                mode="multiple"
+                showSearch
+                placeholder={t("Select the countries you've visited yet")}
+                style={{
+                    width: "100%",
+                }}
+                onChange={onChange}
+                tagRender={flagTagRender}
+                optionLabelProp="label"
+                optionFilterProp="label"
+                value={selected}
+            >
+                {addCountryOptionsForContinent(t("Asia"), countriesByContinents.get("AS"))}
+                {addCountryOptionsForContinent(t("Africa"), countriesByContinents.get("AF"))}
+                {addCountryOptionsForContinent(t("Europe"), countriesByContinents.get("EU"))}
+                {addCountryOptionsForContinent(t("Oceania"), countriesByContinents.get("OC"))}
+                {addCountryOptionsForContinent(t("North America"), countriesByContinents.get("NA"))}
+                {addCountryOptionsForContinent(t("South America"), countriesByContinents.get("SA"))}
+                {addCountryOptionsForContinent(t("Antarctica"), countriesByContinents.get("AN"))}
+            </Select>
+            <Button onClick={clearSelected}>{t("Clear selected countries")}</Button>
+        </div>
     );
-  };
-
-  const addCountryOptionsForContinent = (name, countriesForContinent) => {
-    return (
-      <>
-        <OptGroup label={name} key={name}>
-          {countriesForContinent.map((v) => {
-            const alpha2 = countries
-              .getAlpha2Code(v, locale)
-              .toLocaleLowerCase();
-            const alpha3 = countries
-              .getAlpha3Code(v, locale)
-              .toLocaleLowerCase();
-            const flag = <ReactCountryFlag countryCode={alpha2} />;
-
-            return (
-              <>
-                <Option value={alpha3} label={v} key={alpha3}>
-                  <Space>
-                    <span role="img" aria-label={alpha3}>
-                      {flag}
-                    </span>
-                    {v}
-                  </Space>
-                </Option>
-              </>
-            );
-          })}
-        </OptGroup>
-      </>
-    );
-  };
-
-  return (
-    <div>
-      <Select
-        mode="multiple"
-        showSearch
-        placeholder={t("Select the countries you've visited yet")}
-        style={{
-          width: "100%",
-        }}
-        onChange={onChange}
-        tagRender={flagTagRender}
-        optionLabelProp="label"
-        optionFilterProp="label"
-        value={selected}
-      >
-        {addCountryOptionsForContinent(
-          t("Asia"),
-          countriesByContinents.get("AS"),
-        )}
-        {addCountryOptionsForContinent(
-          t("Africa"),
-          countriesByContinents.get("AF"),
-        )}
-        {addCountryOptionsForContinent(
-          t("Europe"),
-          countriesByContinents.get("EU"),
-        )}
-        {addCountryOptionsForContinent(
-          t("Oceania"),
-          countriesByContinents.get("OC"),
-        )}
-        {addCountryOptionsForContinent(
-          t("North America"),
-          countriesByContinents.get("NA"),
-        )}
-        {addCountryOptionsForContinent(
-          t("South America"),
-          countriesByContinents.get("SA"),
-        )}
-        {addCountryOptionsForContinent(
-          t("Antarctica"),
-          countriesByContinents.get("AN"),
-        )}
-      </Select>
-      <Button onClick={clearSelected}>{t("Clear selected countries")}</Button>
-    </div>
-  );
 }
--- pages/components/language-switcher.js
+++ pages/components/language-switcher.js
@@ -1,25 +1,39 @@
 import { useRouter } from "next/router";
+import { Button, Radio, Select, Space, Tag } from "antd";
+const { Option, OptGroup } = Select;
+import { useTranslation } from "next-i18next";
+import React, { useState } from "react";
 
 export default function LanguageSwitcher() {
-  const router = useRouter();
+    const router = useRouter();
+    const { t } = useTranslation();
+    const locale = t("locale");
 
-  return (
-    <div>
-      <select
-        onChange={(e) =>
-          router.push(
-            {
-              pathname: router.pathname,
-              query: router.query,
-            },
-            null,
-            { locale: e.target.value },
-          )
-        }
-      >
-        <option value="de">Deutsch</option>
-        <option value="en">English</option>
-      </select>
-    </div>
-  );
+    const [size, setSize] = useState(locale);
+
+    const handleSizeChange = (e) => {
+        setSize(e.target.value);
+    };
+    return (
+        <div>
+            <Radio.Group
+                value={size}
+                onChange={(e) => {
+                    setSize(e.target.value);
+
+                    router.push(
+                        {
+                            pathname: router.pathname,
+                            query: router.query,
+                        },
+                        null,
+                        { locale: e.target.value },
+                    );
+                }}
+            >
+                <Radio.Button value="de">Deutsch</Radio.Button>
+                <Radio.Button value="en">English</Radio.Button>
+            </Radio.Group>
+        </div>
+    );
 }
